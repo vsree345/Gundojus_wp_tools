95d5d3174b16283df016bf21a60dbb7f8ffccae3
DMN-sohan <gundojusohan@gmail.com>
2024-12-26 12:01:27 +0530

    Updates

---
+++ precache-manifest.2b8b4f35df48cf1647b1c91c95d7860f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5b281df3682fb6648926ef92e55fec",
+    "url": "/Gundojus_wp_tools/index.html"
+  },
+  {
+    "revision": "6f342439f38767b1e568",
+    "url": "/Gundojus_wp_tools/static/js/2.ba8d04a0.chunk.js"
+  },
+  {
+    "revision": "4d249f42d389787ff562b8c4e1786a99",
+    "url": "/Gundojus_wp_tools/static/js/2.ba8d04a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f68d376a8f879ebf65",
+    "url": "/Gundojus_wp_tools/static/js/main.9b6d61e4.chunk.js"
+  },
+  {
+    "revision": "022c037bf8c298bff89f",
+    "url": "/Gundojus_wp_tools/static/js/runtime-main.984b0878.js"
+  }
+]);
